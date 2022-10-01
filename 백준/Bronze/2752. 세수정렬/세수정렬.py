02a010f23c93cee534ffca387fa90052b6b9ad0d
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-10-01 20:28:40 +0900

    [Bronze IV] Title: 세수정렬, Time: 68 ms, Memory: 30852 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.py
+++ 백준/Bronze/2752. 세수정렬/세수정렬.py
@@ -1,3 +1,8 @@
-a = list(map(int,input().split(" ")))
-a.sort()
-print(a[0] , a[1] , a[2])
+a = list(map(int, input().split(" ")))
+for i in range(len(a)):
+    idx = i
+    for k in range(i+1 , len(a)):
+        if a[idx] > a[k]:
+            a[idx] , a[k] = a[k] , a[idx]
+
+print(a[0],a[1],a[2])
