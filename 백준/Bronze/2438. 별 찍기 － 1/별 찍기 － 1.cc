59ddc99dd64e93f6efbf02016065c3341383f114
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-10-01 11:44:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a , i ,k;
+	scanf("%d",&a);
+	for(i = 1 ; a >= i ; i++)
+	{
+		
+	  	for(k = 1 ; i>= k ; k++)
+	  	{
+	  		printf("*");
+			
+		}
+		printf("\n");
+	}
+}
