9085f56b75063a5ff3d303e846b40ec88307e2a2
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-10-08 17:02:33 +0900

    [Bronze II] Title: 문자열 반복, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,8 +1,7 @@
 s = int(input())
 for i in range(s):
-    a = ''
-    n , answk = input().split(' ')
-    n = int(n)
-    for k in answk: # answk = abc a, b, c
-        a += k * n
-    print(a)  
+  x , y = input().split(" ")
+  x = int(x)
+  for k in y:
+    print(k*x ,end="")
+  print('')
