feb36718dfdba920a1ba996ce5caf763a0d4c11e
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-10-08 16:05:14 +0900

    [Bronze II] Title: 숫자, Time: 148 ms, Memory: 115144 KB -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.py
+++ 백준/Bronze/10093. 숫자/숫자.py
@@ -1,10 +1,10 @@
-a , b = map(int,input().split(" ")) 
+a , b = map(int, input().split(" "))
 if a == b:
-    print(0)
+  print(0)
 else:
-    maaaax = max(a , b)
-    miiiin = min(a , b)
-
-    print(maaaax - miiiin - 1)
-    for l in range(miiiin + 1 , maaaax):
-        print(l , end = " ")
+  m = max(a,b) # 최대범위
+  n = min(a,b) # 최소범위  # (8 , 14) 9 ~ 13 안포함  # [8 ,14] 8 ~ 14 포함된거 
+  box = m - n - 1 # [1 , 10)
+  print(box)
+  for i in range(n +1 , m):
+    print(i , end =' ')
