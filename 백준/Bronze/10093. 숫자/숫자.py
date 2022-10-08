2a657bd87bc974e1c6d536e0db3ff3876dc12c74
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-10-08 11:40:57 +0900

    [Bronze II] Title: 숫자, Time: 132 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.py
+++ 백준/Bronze/10093. 숫자/숫자.py
@@ -1,11 +1,10 @@
-#10093 숫자
-a ,b = map(int,input().split(" "))
-n1 = min(a,b)
-n2 = max(a,b)
-n = abs(n2 - n1 -1)# 범위 
-if n1 == n2:
-  n = 0
-print(n)
+a , b = map(int,input().split(" ")) 
+if a == b:
+    print(0)
+else:
+    maaaax = max(a , b)
+    miiiin = min(a , b)
 
-for i in range(n1+1 , n2 , 1):
-  print(i , end =" ")
+    print(maaaax - miiiin - 1)
+    for l in range(miiiin + 1 , maaaax):
+        print(l , end = " ")
