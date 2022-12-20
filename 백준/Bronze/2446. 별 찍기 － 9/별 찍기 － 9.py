74cc186cfaa58085ee709eb49795b104588d1fcc
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-12-20 16:34:49 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 52 ms, Memory: 30620 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(n , 0 , -1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+for i in range(2 , n+1 , 1):
+    for k in range(n-i):
+        print(" " , end="")
+    for k in range(i):
+        print("*" , end="")
+    for k in range(i-1):
+        print("*" , end="")
+    print()
