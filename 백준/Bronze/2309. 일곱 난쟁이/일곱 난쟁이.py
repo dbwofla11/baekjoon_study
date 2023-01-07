5609a991014efc95090e33572b0aa9862c1e9731
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2023-01-07 12:43:56 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,15 +1,16 @@
-arr=[]
-for i in range(9):
-    arr.append(int(input()))
+h = []
 
 for i in range(9):
-    for j in range(9):
-        if sum(arr)-arr[i]-arr[j]==100:
-            x,y=i,j
-            break
-            
-arr.remove(arr[x])
-arr.remove(arr[y])
-arr.sort()
-for i in arr:
-    print(i)
+    a = int(input())
+    h.append(a)
+
+for i in range(9):
+    for s in range(i + 1 , 9):
+        if sum(h) - (h[i] + h[s]) == 100:
+            x1 = h[i]
+            y1 = h[s]
+
+h.remove(x1)
+h.remove(y1)
+
+print('\n'.join(map(str, sorted(h))))
