0f6ec09eaa27c875812656296b01fccb93cecfe8
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2023-01-10 01:43:15 +0900

    [Silver IV] Title: 역원소 정렬, Time: 464 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.py
@@ -0,0 +1,23 @@
+# 5648 역원소 정렬 
+# import sys 
+
+# nums = list(map(int , input().split(" ")))
+
+element = []
+n , index = 2000000000 , 0 
+while True:
+    if len(element) == n:
+        break
+    tmp_i = 0
+    for i in list(map(int , input().split())):
+        if index == 0 and tmp_i == 0:
+            n = i
+            tmp_i += 1
+            index += 1
+        else:
+            element.append(int(''.join(reversed(str(i)))))
+
+element.sort()
+for i in element:
+    print(i)
+
