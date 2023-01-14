7cc56fb7e1130eade234e80837eb8268247a4a01
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2023-01-14 13:16:38 +0900

    [Silver III] Title: 두 수의 합, Time: 4820 ms, Memory: 125224 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,23 +1,11 @@
-# 3273 두 수의 합 
 
-num = int(input())
+x = int(input())
+y = list(map(int , input().split(' ')))
+z = int(input())
 
-numbers = list(map(int, input().split(' ')))
-numbers.sort()
+cnt = 0
+for i in y:
+    if z - i in y:
+        cnt += 1
 
-sum_num = int(input())
-sum_cnt = 0
-
-L , R = 0 , num - 1
-while L != R:
-    if numbers[L] + numbers[R] == sum_num:
-        sum_cnt += 1
-        L += 1
-    elif numbers[L] + numbers[R] > sum_num:
-        R -= 1
-    
-    else:
-        L += 1
-    
-
-print(sum_cnt)
+print(cnt // 2)
