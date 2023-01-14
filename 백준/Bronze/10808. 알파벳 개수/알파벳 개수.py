c369b5e578a5d61197b49ecca273ff12b30c013f
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2023-01-14 12:38:53 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,5 @@
+apb = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+x = input()
+for i in apb:
+    print(x.count(i) , end = " ")
