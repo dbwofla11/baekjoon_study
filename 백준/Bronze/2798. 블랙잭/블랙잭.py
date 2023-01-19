c361a01a002e1ee1e363fb435bc6118cb56728a5
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2023-01-19 15:51:27 +0900

    [Bronze II] Title: 블랙잭, Time: 92 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,16 +1,10 @@
-# 2798 블랙잭 - 브루트포스 ( 선형 탐색 )
-
-nums , card_nums = map(int, input().split())
-cards = list(map(int , input().split()))
-
-point = 0
-
-for i in range(0 , nums - 2): # 나머지 2장은 중복방지 
-    for k in range(i + 1 , nums - 1):
-        for m in range(k + 1 , nums):
-            if cards[i] + cards[k] + cards[m] > card_nums:
-                continue
-            else:
-                point = max(point , cards[i] + cards[k] + cards[m]) # max를 쓰면 if문 없이 편하게 갱신 가능 
-
-print(point)
+n , m = map(int , input().split(' '))
+x = list(map(int , input().split(' ')))
+max = 0
+for i in range(n - 2):
+    for j in range(i + 1 , n - 1 ):
+        for k in range(j + 1 , n):
+            if x[i] + x[j] + x[k] <= m:
+                if max <= x[i] + x[j] + x[k]:
+                    max = x[i] + x[j] + x[k]
+print(max)
