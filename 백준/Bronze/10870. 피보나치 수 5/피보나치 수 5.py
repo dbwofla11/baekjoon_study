426aa30c229ae0895d1ed7aee8ac54dfaa842c93
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2023-02-04 11:35:51 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,8 +1,11 @@
-def fibo(n):
-    if n <= 1:
-        return n
-    else: return fibo(n-1) + fibo(n-2)
-
+# 재귀 연습 - 피보나치 수열 
 n = int(input())
-answer = fibo(n)
-print(answer)
+
+def fibo(n):
+    if n <= 1: # 재귀 필수 조건 ( 종료 조건 )
+        return n # 리턴이 끝내는거 
+    else:
+        return fibo(n-1) + fibo(n-2) # 계속 함수 불러오기 
+
+
+print(fibo(n))
