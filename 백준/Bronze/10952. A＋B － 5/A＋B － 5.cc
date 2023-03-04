438f1c8484b26c65f33bcbdd46a8e6b798862591
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2023-03-04 11:55:11 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a , b ; 
+	
+	while(true)
+	{
+		scanf("%d %d",&a, &b);
+		
+		
+		if(a == 0 && b == 0)
+		{
+			break;
+		}
+		printf("%d\n",a+b);
+		
+	}
+	return 0 ;
+}
